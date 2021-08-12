bae847cfff717fd94af706c9574cffaf8cd6a25f
Jeerawat Chuachan <jeerawatchu@gmail.com>
2021-08-12 15:18:22 +0700

    Added comments

--- generator.jl
+++ generator.jl
@@ -20,17 +20,20 @@ function collatz_generator(n)
 end
 
 
+# Create 100 arrays from collatz_generator with seed 1 to 100
 y = []
 for i in 1:100
     push!(y, collatz_generator(i))
 end
 
+# Add every array as a layer in the plot
 layers = []
 for y_i in y
     layer_i = layer(x=[i for i in 1:length(y_i)], y=y_i, Geom.line, Geom.point)
     push!(layers, layer_i)
 end
 
+# Export the figure as SVG 
 img = SVG("collatz_plot.svg", 12inch, 8inch)
 fig = plot(layers...)
 draw(img, fig)
